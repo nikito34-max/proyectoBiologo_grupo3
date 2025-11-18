6754cbcbf1bbdb5e0421d797e0e4ac2fff2ae7ea
Narita89 <samara2016salazar@gmail.com>
2025-11-18 08:43:13 -0500

    Update Protozoo.java

--- src/pkBioAnalytics/pkReinoViviente/pkProtista/Protozoo.java
+++ src/pkBioAnalytics/pkReinoViviente/pkProtista/Protozoo.java
@@ -1,14 +1,22 @@
 package pkBioAnalytics.pkReinoViviente.pkProtista;
 
 public abstract class Protozoo extends ReinoProtista { 
- 
+
+
     public Protozoo() {
         super(); 
         super.setNombre("Protozoo desconocido");
         super.setEstaVivo(true); 
     }
-    
-    
+
+   
+    public Protozoo(String nombre, double tamanio) {
+        super();
+        super.setNombre(nombre);
+        super.setTamanio(tamanio);
+        super.setEstaVivo(true);
+    }
+
     public abstract void mover();
 
     public abstract void descansar(int horas);
