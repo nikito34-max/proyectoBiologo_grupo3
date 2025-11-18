763993ea53ee7e8d2ede124852875d1e26bbb99f
Narita89 <samara2016salazar@gmail.com>
2025-11-18 08:42:10 -0500

    Update ReinoProtista.java

--- src/pkBioAnalytics/pkReinoViviente/pkProtista/ReinoProtista.java
+++ src/pkBioAnalytics/pkReinoViviente/pkProtista/ReinoProtista.java
@@ -1,5 +1,43 @@
 package pkBioAnalytics.pkReinoViviente.pkProtista;
-import pkBioAnalytics.pkReinoViviente.SerVivo;
 
-public abstract class ReinoProtista extends SerVivo {
+public abstract class ReinoProtista {
+
+    protected String nombre;
+    protected boolean estaVivo;
+    protected double tamanio;   
+
+    public ReinoProtista() {
+        this.nombre = "Organismo desconocido";
+        this.estaVivo = true;
+        this.tamanio = 1.0;    
+    }
+
+   
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean getEstaVivo() {
+        return estaVivo;
+    }
+
+    public void setEstaVivo(boolean estaVivo) {
+        this.estaVivo = estaVivo;
+    }
+
+
+    public double getTamanio() {
+        return tamanio;
+    }
+
+    public void setTamanio(double tamanio) {
+        this.tamanio = tamanio;
+    }
 }
+
+
