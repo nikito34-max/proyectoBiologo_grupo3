69f6b4ff1c9d4730a905c9f8b2ad76e6c1d287df
Narita89 <samara2016salazar@gmail.com>
2025-11-18 08:41:26 -0500

    Update Rizopodo.java

--- src/pkBioAnalytics/pkReinoViviente/pkProtista/Rizopodo.java
+++ src/pkBioAnalytics/pkReinoViviente/pkProtista/Rizopodo.java
@@ -1,36 +1,22 @@
 package pkBioAnalytics.pkReinoViviente.pkProtista;
 
 public abstract class Rizopodo extends Protozoo {
-    private Double tamanio;
+
     public Rizopodo() {
         super();
-        this.setTamanio(0.0);
     }
+
     public Rizopodo(String nombre, double tamanio) {
-        super();
-        super.setNombre(nombre);
-        this.setTamanio(tamanio);
+        super(nombre, tamanio);
     }
 
-   public void mover() {
-        if (!this.getEstaVivo()) {
-            System.out.println(this.getNombre() + " ya no puede moverse porque está muerto.");
-            return;
-        }
-        System.out.println(this.getNombre() + " se mueve extendiendo sus pseudópodos.");
+    @Override
+    public double getTamanio() {    
+        return this.tamanio;
     }
 
-    public void descansar(int horas) {
-        if (!this.getEstaVivo()) {
-            System.out.println(this.getNombre() + " no puede descansar porque está muerto.");
-            return;
-        }
-        System.out.println(this.getNombre() + " reposa en el fondo del agua durante " + horas + " horas.");
-    }
-    public Double getTamanio() {
-        return tamanio;
-    }
-    public void setTamanio(Double tamanio) {
+    @Override
+    public void setTamanio(double tamanio) {   
         this.tamanio = tamanio;
     }
 }
